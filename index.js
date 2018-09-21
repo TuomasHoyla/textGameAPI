4748125cd31f9910907c7563d34d1365a34059eb
Tuomas Höylä <tuomas.hoyla@probissolutions.fi>
2018-09-21 13:30:59 +0300

    toka kommit

--- index.js
+++ index.js
@@ -2,9 +2,42 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const bodyParser = require('body-parser')
+
+const app = express()
+
+const cors = require('cors')
+
+app.use(cors())
+
+app.use(bodyParser.json())
+
+const logger = (request, response, next) => {
+  console.log('Method:',request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(logger)
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
+app.get('/', function (req, res) {
+  res.send('hello, world!')
+})
+
+const error = (request, response) => {
+  response.status(404).send({error: 'unknown endpoint'})
+}
+
+
+//express()
+//  .use(express.static(path.join(__dirname, 'public')))
+//  .set('views', path.join(__dirname, 'views'))
+//  .set('view engine', 'ejs')
+//  .get('/', (req, res) => res.render('pages/index'))
+//  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
